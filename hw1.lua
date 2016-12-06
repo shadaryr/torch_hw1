130cfdfd667a0d2f3fc5e0ce697d5ffd08b42667
shadaryr <shadaryr@campus.technion.ac.il>
2016-12-07 01:07:48 +0200

    relu-relu-tanh

--- hw1.lua
+++ hw1.lua
@@ -37,12 +37,11 @@ model:add(nn.View(28 * 28)) --reshapes the image into a vector without copy
 end ]]--
 
 model:add(nn.Linear(inputSize, 64))
-model:add(nn.Tanh())
+model:add(nn.ReLU())
 model:add(nn.Linear(64, 128))
 model:add(nn.ReLU())
 model:add(nn.Linear(128, 32))
-model:add(nn.ReLU())
-
+model:add(nn.Tanh())
 
 -- Adding the final layer to the model
 model:add(nn.Linear(layerSize[#layerSize], outputSize))
